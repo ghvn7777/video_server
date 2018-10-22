306494d88dd0479c7e6ae8e594356b02b520148f
kaka company black computer <852826897@qq.com>
2018-10-22 22:00:34 +0800

    home handlers

--- web/handlers.go
+++ web/handlers.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
-	
+import (
+	"github.com/julienschmidt/httprouter"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type HomePage struct {
+	Name string
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	p := &HomePage{Name: "kaka"}
+	t, e := template.ParseFiles("./template/home.html")
+	if e != nil {
+		log.Printf("Parsing template home.html error: %v", e)
+		return
+	}
+	t.Execute(w, p)
+	return
+}
+
+func userhomeHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
 }
--- web/main.go
+++ web/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func RegisterHandler() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/", homeHandler)
+	router.POST("/", homeHandler)
+
+	router.GET("/userhome", userhomeHandler)
+	router.POST("/userhome", userhomeHandler)
+
+	router.POST("/api", apiHandler)
+
+	// 把 template 下的文件自动挂载到 statics 目录下，后面访问方便
+	// 127.0.0.1:8080/statics/*... 其实访问的是 template 文件夹下的文件
+	router.ServeFiles("/statics/*filepath", http.Dir("./template"))
+}
+
 func main() {
-	
+	r := RegisterHandler()
+	http.ListenAndServe(":8080", r)
 }
